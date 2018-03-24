974049793f703f016d07c8e241a1c9318f614344
Kevin Imvoo <imvooz@gmail.com>
2018-03-25 00:58:15 +1100

    Add background colour to Map page.

--- src/map/Map.js
+++ src/map/Map.js
@@ -1,25 +1,26 @@
 import React, { Component } from "react";
 import MapView from "../components/mapview/MapView";
+import { primaryColour } from "../style/AppTheme";
 import { View } from "react-native";
 import BinUI from "./BinUI";
 
 class Map extends Component {
-    static navigationOptions = ({ navigation, navigationOptions }) => {
-        const { params } = navigation.state;
-    
-        return {
-            title: "Map"
-        };
-    }
+  static navigationOptions = ({ navigation, navigationOptions }) => {
+    const { params } = navigation.state;
 
-    render() {
-        return (
-            <View>
-                <MapView height={300} width={"100%"}/>
-                <BinUI />
-            </View>
-        );
-    }
+    return {
+      title: "Map"
+    };
+  };
+
+  render() {
+    return (
+      <View style={{ backgroundColor: primaryColour, height: "100%" }}>
+        <MapView height={300} width={"100%"} />
+        <BinUI />
+      </View>
+    );
+  }
 }
 
 export default Map;
