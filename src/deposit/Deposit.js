7fbfdecde11073b0f582fbd10012a19e1bc7bceb
Kevin Imvoo <imvooz@gmail.com>
2018-03-24 21:38:42 +1100

    Add beautifier to Deposit component.

--- src/deposit/Deposit.js
+++ src/deposit/Deposit.js
@@ -1,28 +1,54 @@
 import React, { Component } from "react";
+import BoxWrapper from "../components/boxwrapper/BoxWrapper";
 import { Text, View, Button } from "react-native";
+import { primaryColour, accentColour } from "../style/AppTheme";
 
 export default class Deposit extends Component {
-    static navigationOptions = ({ navigation, navigationOptions }) => {
-        const { params } = navigation.state;
+  constructor(props) {
+    super(props);
 
-        return {
-            title: "Deposit Screen"
-        };
-    }
+    this.state = {
+      currentWeight: 0
+    };
+  }
 
-    render() {
-        return(
-            <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-                <Text> wowee </Text>
-                <Button
-                    onPress={() => this.props.navigation.navigate("Confirmation")}
-                    title="push me daddy"
-                    color="red"
-                    accessibilityLabel="yes baby do it" />
-                
-            </View>
-        );
-    }
+  static navigationOptions = ({ navigation, navigationOptions }) => {
+    const { params } = navigation.state;
 
+    return {
+      title: "Deposit Screen"
+    };
+  };
 
+  formatWeight() {
+    return this.state.currentWeight.toFixed(2).toString() + " kg";
+  }
+
+  render() {
+    let weight = (
+      <Text style={{ fontSize: 72, color: accentColour }}>
+        {this.formatWeight()}
+      </Text>
+    );
+
+    return (
+      <View
+        style={{
+          flex: 1,
+          justifyContent: "center",
+          alignItems: "center",
+          backgroundColor: primaryColour,
+          paddingHorizontal: 20
+        }}
+      >
+        <BoxWrapper child={weight} />
+        <Button
+          onPress={() => this.props.navigation.navigate("Confirmation")}
+          title="push me daddy"
+          color="red"
+          accessibilityLabel="yes baby do it"
+        />
+      </View>
+    );
+  }
 }
