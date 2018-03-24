812dc976bd04542443f56b07bc56c243e3444f22
Kevin Imvoo <imvooz@gmail.com>
2018-03-24 15:39:17 +1100

    Add MapView component (redirects to Google Maps).

---
+++ src/components/mapview/MapView.js
@@ -0,0 +1,24 @@
+import React from "react";
+import { View } from "react-native";
+import { MapView as ExpoMapView } from "expo";
+
+export default class MapView extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return (
+      <View style={{ width: this.props.width, height: this.props.height }}>
+        <ExpoMapView
+          style={{ width: "100%", height: "100%" }}
+          initialRegion={{
+            latitude: 37.78825,
+            longitude: -122.4324,
+            latitudeDelta: 0.0922,
+            longitudeDelta: 0.0421
+          }}
+        />
+      </View>
+    );
+  }
+}
--- src/dashboard/Dashboard.js
+++ src/dashboard/Dashboard.js
@@ -1,16 +1,29 @@
 import React from "react";
 import NameBox from "./namebox/NameBox";
 import DollarBox from "./dollarbox/DollarBox";
-import { Text, View } from "react-native";
+import MapView from "../components/mapview/MapView";
+import { StyleSheet, Text, View } from "react-native";
 
 export default class Dashboard extends React.Component {
   render() {
     return (
-      <View>
+      <View style={styles.view}>
         <Text>Dashboard</Text>
         <NameBox name="Matty" />
         <DollarBox amount={50} />
+
+        <MapView height={300} width={"100%"} />
       </View>
     );
   }
 }
+
+const styles = StyleSheet.create({
+  view: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+    width: "100%",
+    height: "100%"
+  }
+});
